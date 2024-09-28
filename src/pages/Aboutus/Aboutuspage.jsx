022192936c4061f821498b47fcc94d2cf09bfca9
Shubham <shubhamsinghyadav0890@gmail.com>
2024-09-28 17:29:31 +0530

    code

---
+++ src/pages/Aboutus/Aboutuspage.jsx
@@ -0,0 +1,106 @@
+
+import React from 'react'
+import './Aboutuspage.css';
+
+
+
+export default function Aboutuspage() {
+    return (
+        <>
+            <nav>
+                <div className="navbar">
+                    <ul>
+                            <li><a href="#">EVENTS</a></li>
+                            <li><a href="#">OUR TEAM</a></li>
+                            <li><a href="#"><img src="D:\Layer_x0020_1.jpg"></img></a></li>
+                            <li><a href="#">ABOUT US</a></li>
+                       
+                            <li><a href="#">SIGN IN</a></li>
+                    </ul>
+                </div>
+
+            </nav>
+            <div className="Aboutsection">
+                <div className="aboutus">
+                    <h2>About Us</h2>
+                </div>
+                <div className="containermain1">
+                    <div className="container1">
+                        <h3 align="center">About Youthopia</h3>
+
+                    </div>
+                    <div className="container2">
+                        <h3 align="center">Youthopia Logo</h3>
+
+                    </div>
+                </div>
+                <div className="associationtext">
+                    <h2 align="center">  IN ASSOCIATION WITH </h2>
+                </div>
+                <div className="sponsorlogo">
+                    <div className="logo1">
+
+                    </div>
+
+                    <div className="logo2">
+
+                    </div>
+                    <div className="logo3">
+
+                    </div>
+                    <div className="logo4">
+
+                    </div>
+                    <div className="logo5">
+
+                    </div>
+                    <div className="logo6">
+
+                    </div>
+
+                </div>
+
+                <div className="containermain2">
+                    <div className="container3">
+                        <h3 align="center">DIT Logo</h3>
+
+                    </div>
+                    <div className="container4">
+                        <h3 align="center">About Dit University</h3>
+                        <button id="knowmore" type="submit">Know More</button>
+
+                    </div>
+                </div>
+
+                <div className="fromthedesktext">
+                    <h2 align="center">  FROM THE DESK</h2>
+                </div>
+
+                <div className="containermain3">
+                    <div className="container5">
+                        <h3 align="center">DIT Logo</h3>
+
+                    </div>
+                    <div className="container6">
+                        <h3 align="center">About Dit University</h3>
+
+
+                    </div>
+                </div>
+
+                <div className="containermain4">
+
+                </div>
+
+
+
+
+
+
+
+            </div>
+        </>
+    )
+}
+
+
