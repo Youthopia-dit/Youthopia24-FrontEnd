f90fab680b2654a92052797233f23ff02e50b537
AJ-9005 <sharmaaanj1@gmail.com>
2024-09-29 17:07:52 +0530

    main part done

--- src/pages/Profilepage/profilepage.jsx
+++ src/pages/Profilepage/profilepage.jsx
@@ -4,14 +4,45 @@ function ProfilePage(){
     return(
         <div className="ProfilePage">
             <div className="info">
+                <div>
                 <div className="image" >
                     <h3 className="heading">Profile photo</h3>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="inside"></img>
+                </div>
+                <div className="contact">
+                    <h2>Phone</h2>
+                    <h3>1234567890</h3>
+                    <br />
+                    <h2>Email</h2>
+                    <h3>example@gmail.com</h3>
+                </div>
                 </div>
                 <div className="mainheading">
-                    <h1>Your Name</h1>
-                    <h2>Lallu Prasad</h2>
-                    <h1>College</h1>
-                    <h2>DIT</h2>
+                    <h2>Your Name</h2>
+                    <h3>Name</h3>
+                    <h2>College</h2>
+                    <h3>DIT</h3>
+                    <h2>College ID</h2>
+                    <h3>ID</h3>
+                    <h2>Branch</h2>
+                    <h3>CSE</h3>
+                    <h2>Year</h2>
+                    <h3>2nd</h3>
+                    <button className="button">Logout</button>
+                </div>
+            </div>
+            <br />
+            <br />
+            <div>
+                <h1 className="registered">Registered events</h1>
+                <div className="registered">
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
+                    <img src="https://m.media-amazon.com/images/I/61QPLVPgSOL._SX679_.jpg" className="events"></img>
                 </div>
             </div>
         </div>
