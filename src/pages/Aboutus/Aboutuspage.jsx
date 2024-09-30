bc1c2cc2fc1d7035524665cca828415da5ef58b2
Shubham <shubhamsinghyadav0890@gmail.com>
2024-09-30 18:17:39 +0530

    done

--- src/pages/Aboutus/Aboutuspage.jsx
+++ src/pages/Aboutus/Aboutuspage.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import './Aboutuspage.css';
 import ditlogo from '../../logos/ditlogo.jpg';
-import download from '../../logos/download.jpeg';
+
 import download1 from '../../logos/download1.jpeg';
 
 
@@ -14,12 +14,12 @@ export default function Aboutuspage() {
             <nav>
                 <div className="navbar">
                     <ul>
-                            <li><a href="#">EVENTS</a></li>
-                            <li><a href="#">OUR TEAM</a></li>
-                            <li><a href="#"><img src={ditlogo}></img></a></li>
-                            <li><a href="#">ABOUT US</a></li>
-                       
-                            <li><a href="#">SIGN IN</a></li>
+                        <li><a href="#">EVENTS</a></li>
+                        <li><a href="#">OUR TEAM</a></li>
+                        <li><a href="#"><img src={ditlogo}></img></a></li>
+                        <li><a href="#">ABOUT US</a></li>
+
+                        <li><a href="#">SIGN IN</a></li>
                     </ul>
                 </div>
 
@@ -38,7 +38,7 @@ export default function Aboutuspage() {
 
                     </div>
                     <div className="container2">
-                       <img src={download1}></img>
+                        <img src={download1}></img>
 
                     </div>
                 </div>
@@ -47,43 +47,46 @@ export default function Aboutuspage() {
                 </div>
                 <div className="sponsorlogo">
                     <div className="logo1">
-                    <img src={download1}></img>
+                        <img src={download1}></img>
 
                     </div>
 
                     <div className="logo2">
-                    <img src={download1}></img>
+                        <img src={download1}></img>
 
                     </div>
                     <div className="logo3">
-                    <img src={download1}></img>
-                    
+                        <img src={download1}></img>
 
-                    </div> 
+
+                    </div>
                     <div className="logo4">
-                    <img src={download1}></img>
+                        <img src={download1}></img>
 
                     </div>
-                     <div className="logo5">
-                     <img src={download1}></img>
+                    <div className="logo5">
+                        <img src={download1}></img>
 
                     </div>
-                    {/* <div className="logo6">
 
-                    </div>  */}
 
                 </div>
 
                 <div className="containermain2">
                     <div className="container3">
-                        <h3 align="center">DIT Logo</h3>
+
                         <img src={download1}></img>
 
                     </div>
                     <div className="container4">
-                        <h3 align="center">About Dit University</h3>
-                        <img src={download1}></img>
-                        <button id="knowmore" type="submit">Know More</button>
+
+                        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus in ipsum, sit maxime adipisci distinctio molestias. Distinctio, et. Consectetur, provident atque excepturi recusandae suscipit debitis nesciunt fugiat corrupti magnam exercitationem?
+                            Facilis rerum error cupiditate accusamus maxime, ad consequatur quo, quam pariatur molestiae fuga nisi eius. Nam facere, quisquam nobis eaque necessitatibus labore cumque debitis similique aspernatur, odio maiores corporis rerum?
+                            Perspiciatis alias, aut enim ipsa, blanditiis necessitatibus, minima doloribus ut consequatur hic cupiditate neque placeat deleniti exercitationem iste eveniet repellat rerum possimus adipisci? Eligendi rerum temporibus odit necessitatibus laboriosam magnam.
+                            Possimus quo neque blanditiis, ex distinctio facere inventore esse asperiores, in aperiam adipisci consequuntur temporibus doloremque quasi et error commodi fugit! Dolor excepturi et placeat provident labore pariatur commodi aut.</h3>
+                        
+                        <div className="knowmore">
+                        <button type="submit">Know More</button></div>
 
                     </div>
                 </div>
@@ -92,29 +95,47 @@ export default function Aboutuspage() {
                     <h2 align="center">  FROM THE DESK</h2>
                 </div>
 
+
+
+
                 <div className="containermain3">
                     <div className="container5">
-                        <h3 align="center">DIT Logo</h3>
+
                         <img src={download1}></img>
 
                     </div>
                     <div className="container6">
-                        <h3 align="center">About Dit University</h3>
-                        <img src={download1}></img>
+
+                        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus in ipsum, sit maxime adipisci distinctio molestias. Distinctio, et. Consectetur, provident atque excepturi recusandae suscipit debitis nesciunt fugiat corrupti magnam exercitationem?
+                            Facilis rerum error cupiditate accusamus maxime, ad consequatur quo, quam pariatur molestiae fuga nisi eius. Nam facere, quisquam nobis eaque necessitatibus labore cumque debitis similique aspernatur, odio maiores corporis rerum?
+                            Perspiciatis alias, aut enim ipsa, blanditiis necessitatibus, minima doloribus ut consequatur hic cupiditate neque placeat deleniti exercitationem iste eveniet repellat rerum possimus adipisci? Eligendi rerum temporibus odit necessitatibus laboriosam magnam.
+                            Possimus quo neque blanditiis, ex distinctio facere inventore esse asperiores, in aperiam adipisci consequuntur temporibus doloremque quasi et error commodi fugit! Dolor excepturi et placeat provident labore pariatur commodi aut.</h3>
 
 
                     </div>
                 </div>
 
                 <div className="containermain4">
-                <img src={download1}></img>
+                    <div className="container7">
+                        <h3 align="center">About Youthopia
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
+                            Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim unde voluptate perferendis voluptates ullam nemo similique, facilis earum eligendi aspernatur. Facilis minus earum officia magnam perspiciatis?
+                            Quae quibusdam maxime, quis repellat, tempore impedit assumenda velit aliquam autem nisi porro? Porro autem magni officia minus et numquam suscipit doloremque quam officiis quo assumenda, placeat laboriosam. Saepe, odit.
+                        </h3>
 
+                    </div>
+                    <div className="container8">
+                        <img src={download1}></img>
+
+                    </div>
                 </div>
 
 
 
+                <div className="containermain5">
+                    <img src={download1}></img>
 
-
+                </div>
 
 
             </div>
