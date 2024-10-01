d44e01623e412c44805aa5c2e7df174fc8343a7b
Shubham <shubhamsinghyadav0890@gmail.com>
2024-10-01 19:00:14 +0530

    done

--- src/pages/Aboutus/Aboutuspage.jsx
+++ src/pages/Aboutus/Aboutuspage.jsx
@@ -5,141 +5,88 @@ import ditlogo from '../../logos/ditlogo.jpg';
 
 import download1 from '../../logos/download1.jpeg';
 
-
+var list = [{ id: 1, img: download1 },
+{ id: 2, img: download1 },
+{ id: 3, img: download1 },
+{ id: 4, img: download1 },
+{ id: 5, img: download1 }];
 
 
 export default function Aboutuspage() {
+
     return (
-        <>
-            <nav>
-                <div className="navbar">
-                    <ul>
-                        <li><a href="#">EVENTS</a></li>
-                        <li><a href="#">OUR TEAM</a></li>
-                        <li><a href="#"><img src={ditlogo}></img></a></li>
-                        <li><a href="#">ABOUT US</a></li>
 
-                        <li><a href="#">SIGN IN</a></li>
-                    </ul>
+
+
+        <div div className="Aboutsection">
+            <h2>About Us </h2>
+            <div className="about-youthopia-main">
+                <h3 align="center">About Youthopia
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
+                    Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim unde voluptate perferendis voluptates ullam nemo similique, facilis earum eligendi aspernatur. Facilis minus earum officia magnam perspiciatis?
+                    Quae quibusdam maxime, quis repellat, tempore impedit assumenda velit aliquam autem nisi porro? Porro autem magni officia minus et numquam suscipit doloremque quam officiis quo assumenda, placeat laboriosam. Saepe, odit.
+                </h3>
+                <img src={download1}></img>
                 </div>
+            
 
-            </nav>
-            <div className="Aboutsection">
-                <div className="aboutus">
-                    <h2>About Us</h2>
-                </div>
-                <div className="containermain1">
-                    <div className="container1">
-                        <h3 align="center">About Youthopia
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
-                            Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim unde voluptate perferendis voluptates ullam nemo similique, facilis earum eligendi aspernatur. Facilis minus earum officia magnam perspiciatis?
-                            Quae quibusdam maxime, quis repellat, tempore impedit assumenda velit aliquam autem nisi porro? Porro autem magni officia minus et numquam suscipit doloremque quam officiis quo assumenda, placeat laboriosam. Saepe, odit.
-                        </h3>
+            <h2 align="center" className="associationtext">  IN ASSOCIATION WITH </h2>
 
-                    </div>
-                    <div className="container2">
-                        <img src={download1}></img>
-
-                    </div>
-                </div>
-                <div className="associationtext">
-                    <h2 align="center">  IN ASSOCIATION WITH </h2>
-                </div>
-                <div className="sponsorlogo">
-                    <div className="logo1">
-                        <img src={download1}></img>
-
-                    </div>
-
-                    <div className="logo2">
-                        <img src={download1}></img>
-
-                    </div>
-                    <div className="logo3">
-                        <img src={download1}></img>
+            <div className="sponsorlogo">
+                {list.map(el => <img className='sponsorImage' src={el.img}></img>)}
+            </div>
 
 
-                    </div>
-                    <div className="logo4">
-                        <img src={download1}></img>
+            <div className="containermain2">
+                <img src={download1}></img>
+                <h3 align="center">About Youthopia
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
+                    Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim unde voluptate perferendis voluptates ullam nemo similique, facilis earum eligendi aspernatur. Facilis minus earum officia magnam perspiciatis?
+                    Quae quibusdam maxime, quis repellat, tempore impedit assumenda velit aliquam autem nisi porro? Porro autem magni officia minus et numquam suscipit doloremque quam officiis quo assumenda, placeat laboriosam. Saepe, odit.
+                
+                </h3>
+                <button type="submit" className="knowmore">Know More</button>
 
-                    </div>
-                    <div className="logo5">
-                        <img src={download1}></img>
-
-                    </div>
-
-
-                </div>
-
-                <div className="containermain2">
-                    <div className="container3">
-
-                        <img src={download1}></img>
-
-                    </div>
-                    <div className="container4">
-
-                        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus in ipsum, sit maxime adipisci distinctio molestias. Distinctio, et. Consectetur, provident atque excepturi recusandae suscipit debitis nesciunt fugiat corrupti magnam exercitationem?
-                            Facilis rerum error cupiditate accusamus maxime, ad consequatur quo, quam pariatur molestiae fuga nisi eius. Nam facere, quisquam nobis eaque necessitatibus labore cumque debitis similique aspernatur, odio maiores corporis rerum?
-                            Perspiciatis alias, aut enim ipsa, blanditiis necessitatibus, minima doloribus ut consequatur hic cupiditate neque placeat deleniti exercitationem iste eveniet repellat rerum possimus adipisci? Eligendi rerum temporibus odit necessitatibus laboriosam magnam.
-                            Possimus quo neque blanditiis, ex distinctio facere inventore esse asperiores, in aperiam adipisci consequuntur temporibus doloremque quasi et error commodi fugit! Dolor excepturi et placeat provident labore pariatur commodi aut.</h3>
-                        
-                        <div className="knowmore">
-                        <button type="submit">Know More</button></div>
-
-                    </div>
-                </div>
-
-                <div className="fromthedesktext">
-                    <h2 align="center">  FROM THE DESK</h2>
-                </div>
+            </div>
 
 
+            <h2 align="center" className="fromthedesktext">  FROM THE DESK</h2>
+
+            <div className="containermain3">
+
+                <img src={download1}></img>
 
 
-                <div className="containermain3">
-                    <div className="container5">
+                <h3 align="center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus in ipsum, sit maxime adipisci distinctio molestias. Distinctio, et. Consectetur, provident atque excepturi recusandae suscipit debitis nesciunt fugiat corrupti magnam exercitationem?
+                    Facilis rerum error cupiditate a labore cumque debitis similique r sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
+                    Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim aspernatur, odio maiores corporis rerum?</h3>
 
-                        <img src={download1}></img>
-
-                    </div>
-                    <div className="container6">
-
-                        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus in ipsum, sit maxime adipisci distinctio molestias. Distinctio, et. Consectetur, provident atque excepturi recusandae suscipit debitis nesciunt fugiat corrupti magnam exercitationem?
-                            Facilis rerum error cupiditate accusamus maxime, ad consequatur quo, quam pariatur molestiae fuga nisi eius. Nam facere, quisquam nobis eaque necessitatibus labore cumque debitis similique aspernatur, odio maiores corporis rerum?
-                            Perspiciatis alias, aut enim ipsa, blanditiis necessitatibus, minima doloribus ut consequatur hic cupiditate neque placeat deleniti exercitationem iste eveniet repellat rerum possimus adipisci? Eligendi rerum temporibus odit necessitatibus laboriosam magnam.
-                            Possimus quo neque blanditiis, ex distinctio facere inventore esse asperiores, in aperiam adipisci consequuntur temporibus doloremque quasi et error commodi fugit! Dolor excepturi et placeat provident labore pariatur commodi aut.</h3>
-
-
-                    </div>
-                </div>
-
-                <div className="containermain4">
-                    <div className="container7">
-                        <h3 align="center">About Youthopia
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
-                            Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim unde voluptate perferendis voluptates ullam nemo similique, facilis earum eligendi aspernatur. Facilis minus earum officia magnam perspiciatis?
-                            Quae quibusdam maxime, quis repellat, tempore impedit assumenda velit aliquam autem nisi porro? Porro autem magni officia minus et numquam suscipit doloremque quam officiis quo assumenda, placeat laboriosam. Saepe, odit.
-                        </h3>
-
-                    </div>
-                    <div className="container8">
-                        <img src={download1}></img>
-
-                    </div>
-                </div>
-
-
-
-                <div className="containermain5">
-                    <img src={download1}></img>
-
-                </div>
 
 
             </div>
-        </>
+
+            <div className="containermain4">
+                <h3 align="center">About Youthopia
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magnam velit perferendis nobis aliquam natus maxime magni quidem placeat, tempora quod esse adipisci cum ullam, mollitia eveniet dicta rem inventore!
+                    Esse, debitis animi. Autem sunt natus in, possimus tempora asperiores, obcaecati dicta enim unde voluptate perferendis voluptates ullam nemo similique, facilis earum eligendi aspernatur. Facilis minus earum officia magnam perspiciatis?
+                    Quae quibusdam maxime, quis repellat, tempore impedit assumenda velit aliquam autem nisi porro? Porro autem magni officia minus et numquam suscipit doloremque quam officiis quo assumenda, placeat laboriosam. Saepe, odit.
+                </h3>
+                <img src={download1}></img>
+            </div>
+
+
+
+            <div className="containermain5">
+                <img src={download1}></img>
+
+            </div>
+            </div>
+    
+      
+
+
+
+
     )
 }
 
