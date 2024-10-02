5194e65742449be61f0f5f4101e517c07dcd1285
AJ-9005 <sharmaaanj1@gmail.com>
2024-10-02 21:28:23 +0530

    coming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import './App.css'
 import ProfilePage from './pages/Profilepage/profilepage'
+import ComingSoon from './pages/Coming soon/ComingSoon'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/profile" element={<ProfilePage />}/>
+        <Route path="/comingsoon" element={<ComingSoon />}/>
       </Routes>
     </BrowserRouter>
     </>
---
+++ src/pages/Coming soon/ComingSoon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './ComingSoon.css';
+import logo from './DITU-PRIMARY-LOGO-AllRev (1).png'
+function  ComingSoon(){
+    return(
+        <div className="wholepage">
+            <div className="heading">
+                <img src={logo} className="logo" alt="DIT logo"/>
+            </div>
+            <br />
+            <br />
+            <h2 className="presents">PRESENTS</h2>
+            <h1 className="youthopia">YOUTHOPIA 24</h1>
+            <br />
+            <br />
+            <div className="container">
+                <p className="scroll">Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.</p>
+                {/* <p className="scroll2">Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.Coming soon.</p> */}
+            </div>
+        </div>
+    )
+}
+export default ComingSoon
