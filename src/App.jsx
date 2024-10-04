a00c047d94c01b1745e92ef84abac064e71c3783
Gourii02 <gourikachakarverty@gmail.com>
2024-10-04 08:42:49 +0530

    Signup page 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import IndividualEvent from './pages/Event/Individual/Individualpage'
 import Loginpage from './pages/Login/Loginpage'
 import Signuppage from './pages/Signup/Signuppage'
+import Signup1 from './pages/Signup/Signup1'
 function App() {
 
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/individualpage" element={<IndividualEvent />}/>
         <Route path="/Loginpage" element={<Loginpage />}/>
         <Route path="/Signuppage" element={<Signuppage />}/>
+        <Route path="/Signup1" element={<Signup1 />}/>
       </Routes>
     </BrowserRouter>
     </>
---
+++ src/pages/Signup/Signup1.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Signup1.css"
+function Signup1() {
+    return (<>
+    <div class="card">
+   <img src="" alt="" />
+
+
+    <div className="signup-form">
+      <h2>Just a few details to get started!</h2>
+      <a href="">Are you a DIT student?</a>
+      <form>
+        <input type="text" placeholder="College Name" />
+        <input type="text" placeholder="College ID" />
+        <input type="text" placeholder="Year" />
+        <input type="text" placeholder="Branch" />
+        
+        <button type="submit">SIGN UP</button>
+      </form>
+    </div>
+    </div>
+
+
+    
+    </>   
+    );
+}
+
+export default Signup1;
