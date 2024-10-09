1a81de271519c7f95aad5a0ba0976e62ea1df37e
tanya-2002 <tanyaaggarwal2002@gmail.com>
2024-10-09 23:46:59 +0530

    gallery made

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import './App.css'
 import ProfilePage from './pages/Profilepage/profilepage'
 import ComingSoon from './pages/Coming soon/ComingSoon'
+import Gallery from './pages/Gallery/gallery';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/profile" element={<ProfilePage />}/>
         <Route path="/" element={<ComingSoon />}/>
+        <Route path="/gallery" element={<Gallery />}/>
       </Routes>
     </BrowserRouter>
     </>
