7dd2b0fe859e22c5349dc15c77dbbde8c29be18e
tanya-2002 <tanyaaggarwal2002@gmail.com>
2024-10-13 00:48:06 +0530

    newb

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import './App.css'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Aboutuspage from './Pages/Aboutus/Aboutuspage'
 import ProfilePage from './pages/Profilepage/profilepage'
 import ComingSoon from './pages/Coming soon/ComingSoon'
 import Gallery from './pages/Gallery/gallery';
@@ -14,9 +16,13 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path="/profile" element={<ProfilePage />}/>
         <Route path="/" element={<ComingSoon />}/>
         <Route path="/gallery" element={<Gallery />}/>
+=======
+        <Route path="/about" element={<Aboutuspage/>}/>
+>>>>>>> origin/yadav
       </Routes>
     </BrowserRouter>
     </>
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,22 +1,21 @@
-import "./navbar.css";
-import DIT from "../../assets/ditu-full.png";
+import React from 'react'
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <ul className="navbar-links right-link">
-                <li><a href="#events">Events</a></li>
-                <li><a href="#team">Our Team</a></li>
-            </ul>
-            <div className="logo">
-                <img src={DIT} alt="DIT University Logo" />
-            </div>
-            <ul className="navbar-links left-link">
-                <li><a href="#about">About Us</a></li>
-                <li><a href="#signin">Sign In</a></li>
-            </ul>
-        </nav>
-    );
-};
+export default function Navbar() {
+  return (
+    <div>
+                  <nav>
+                <div className="navbar">
+                    <ul>
+                        <li><a href="#">EVENTS</a></li>
+                        <li><a href="#">OUR TEAM</a></li>
+                        <li><a href="#"><img src={ditlogo}></img></a></li>
+                        <li><a href="#">ABOUT US</a></li>
 
-export default Navbar;
+                        <li><a href="#">SIGN IN</a></li>
+                    </ul>
+                </div>
+
+            </nav>
+    </div>
+  )
+}
