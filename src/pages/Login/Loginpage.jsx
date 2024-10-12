01171cc9911c1af8295fe07553b943bc0c03b67a
Ashi-Verma1758 <ashiverma1758@gmail.com>
2024-10-12 17:10:33 +0530

    <login page done>

--- src/pages/Login/Loginpage.jsx
+++ src/pages/Login/Loginpage.jsx
@@ -1,30 +1,64 @@
-import React from 'react';
-import "./Loginpage.css"
-function Loginpage() {
-    return (<>
-    <section class="img">
-  
-  <div class="img">
-   
-  </div>
-</section>
-   <h1> Welcome Back </h1>
-   <p>Please login to continue</p>
-   <form>
-    <div >
-   <p><input id="button" type="text" name="username" placeholder="username" /></p> 
-    <p><input id="button" type="number" name="username" placeholder="password" /></p>
-    <div class="remem"><a><input type="checkbox" name="checkbox"/>Remember me</a>
-       <a>forgot password</a>
-   </div>
+import React from "react";
+import "./Loginpage.css";
+import ab from "../../assets/ab.png";
+import youthopia_logo from "../../assets/youthopia-logo.png";
 
-    <p><input id="button" class="button" type="button"  value="Login"></input></p> 
-    </div>
-    
-   </form>
-   
-    </>   
-    );
+function Loginpage() {
+  return (
+    <>
+      <div className="main">
+        <div class="background">
+          <img src={ab} alt="bg-page" id="ab" />
+        </div>
+       
+          <div>
+            <img src={youthopia_logo} alt="logo" id="logo" />
+          </div>
+        
+        <div class="bordernp">
+          <h1> Welcome Back </h1>
+          <h3>Login to continue</h3>
+          <form>
+            <div>
+              <p>
+                <input
+                  id="button"
+                  type="text"
+                  name="username"
+                  placeholder="  Name :"
+                />
+              </p>
+              <p>
+                <input
+                  id="button"
+                  type="password" 
+                  name="password"
+                  placeholder="  Password :"
+                  
+                />
+              </p>
+              <div class="remem">
+                <a href="#sigin">
+                  Sign in
+                </a>
+                <a href="#sigin">forgot password</a>
+              </div>
+
+              <p class="Login">
+                <input
+                  id="button"
+                  class="button"
+                  type="button"
+                  value="Login"
+                  class="h"
+                ></input>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Loginpage;
