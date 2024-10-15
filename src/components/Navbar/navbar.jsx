f6d2bbf7d675ade1aef8c61df1dfec9013ab9e5d
tanya-2002 <tanyaaggarwal2002@gmail.com>
2024-10-15 15:38:08 +0530

    navbar responsive

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,23 +1,50 @@
 import "./navbar.css";
 import DIT from "../../assets/ditu-full.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { MdClose } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Navbar = () => {
+function Navbar() {
+    const [navOpen, setNavOpen] = useState(false); 
+
+    const toggleNav = () => {
+        setNavOpen(!navOpen); 
+    };
+
     return (
-        <nav className="navbar">
-            <ul className="navbar-links right-link">
-                <li><Link to="/comingsoon">Events</Link></li>
-                <li><Link to="/comingsoon">Our Team</Link></li>
-            </ul>
-            <div className="logo">
-                <img src={DIT} alt="DIT University Logo" />
+        <nav>
+            <div className={`navbar ${navOpen ? "show-nav" : ""}`}>
+                <ul className="navbar-links right-link">
+                    <Link to="/comingsoon" className="noburger"><li onClick={toggleNav}>Events</li></Link>
+                    <Link to="/comingsoon" className="noburger"><li onClick={toggleNav}>Our Team</li></Link>
+                </ul>
+                <div className="logo">
+                    <img src={DIT} alt="DIT University Logo" />
+                </div>
+                <ul className="navbar-links left-link">
+                <Link to="/comingsoon" className="hamburger"><li onClick={toggleNav}>Events</li></Link>
+                <Link to="/comingsoon" className="hamburger"><li onClick={toggleNav}>Our Team</li></Link>
+                    <Link to="/about"><li onClick={toggleNav}>About Us</li></Link>
+                    <Link to="/comingsoon"><li onClick={toggleNav}>Sign In</li></Link>
+                </ul>
+
+                
+                {navOpen && (
+                    <button className="nav-button nav-close-button" onClick={toggleNav}>
+                        <MdClose size={30} />
+                    </button>
+                )}
+                {!navOpen && (
+                <button className="nav-button" onClick={toggleNav}>
+                    <GiHamburgerMenu size={30} />
+                </button>
+            )}
             </div>
-            <ul className="navbar-links left-link">
-                <li><Link to="/about">About Us</Link></li>
-                <li><Link to="/comingsoon">Sign In</Link></li>
-            </ul>
+
+            
         </nav>
     );
-};
+}
 
 export default Navbar;
