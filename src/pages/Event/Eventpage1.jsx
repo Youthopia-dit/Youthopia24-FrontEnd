60e274ded5818eee7316a2161748dc52306db287
Rudraksh Gupta <rudrakshgupta40@gmail.com>
2024-10-15 11:45:42 +0530

    Events Page Background Done

--- src/pages/Event/Eventpage1.jsx
+++ src/pages/Event/Eventpage1.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import './Eventpage1.css';
-import img from  '../../assets/img.png';
-import img1 from  '../../assets/img1.png';
-import img2 from '../../assets/img2.png';
+import img from '../../assets/img.png';
+import img1 from '../../assets/Gradient_3.png';
+import img2 from '../../assets/Gradient_1.png';
 import img3 from '../../assets/img3.png';
-import img4 from '../../assets/img4.png';
+import img4 from '../../assets/Gradient_2.png';
 import EventCard from '../../components/eventcard/eventcard';
 import bgevent from '../../../src/assets/bgevent.png'; // Top part
 import BGImage2 from '../../../src/assets/Events-Elements/Group 63.png'; // Bottom part
@@ -25,7 +25,7 @@ function Eventpage(props) {
   // Event data categorized by type
   const eventTypes = {
     Technical: Array.from({ length: 12 }, (_, index) => createEvent('Technical', index)),
-    Cultural: Array.from({ length: 12}, (_, index) => createEvent('Cultural', index)),
+    Cultural: Array.from({ length: 12 }, (_, index) => createEvent('Cultural', index)),
     Informal: Array.from({ length: 12 }, (_, index) => createEvent('Informal', index)),
   };
 
@@ -48,50 +48,50 @@ function Eventpage(props) {
     );
   };
 
-  return (  
-    
+  return (
+
     <div className="elements">
       <div className="bgelemets">
-   
-{/* <img src={img1} alt="bg1" id='bg' className='bg1' /> */}
-<img src={img2} alt="bg2" id='bg' className='bg2' />
-<img src={img3} alt="bg3" id='bg' className='bg3' />
-<img src={img4} alt="bg4" id='bg' className='bg4' />
 
-    <div className="events-page">
-      {/* Background Images */}
-      <div className="background">
-        <img src={bgevent} alt="Background Top" className="background" />
-        {/* <img src={BGImage2} alt="Background Bottom" className="background-bottom" /> */}
-      </div>
+        <img src={img1} alt="bg1" id='bg1' className='bg1' />
+        <img src={img2} alt="bg2" id='bg2' className='bg2' />
+        <img src={img3} alt="bg3" id='bg3' className='bg3' />
+        <img src={img4} alt="bg4" id='bg4' className='bg4' />
 
-      <div className="events">
-        <img src={Youthopia} alt="logo" className='youthopia-logo'></img>
-        <header>EVENTS {props.name}</header>
-        <p>These are our main Events.</p>
-
-        <main className="clubs">
-          {/* Tab Buttons */}
-          <div className="tab-buttons">
-            {Object.keys(eventTypes).map((tab) => (
-              <button
-                key={tab}
-                className={activeTab === tab ? 'active' : ''}
-                onClick={() => setActiveTab(tab)}
-                disabled={activeTab === tab}
-                aria-label={`Show ${tab} events`}
-              >
-                {tab}
-              </button>
-            ))}
+        <div className="events-page">
+          {/* Background Images */}
+          <div className="background">
+            <img src={bgevent} alt="Background Top" className="background" />
+            {/* <img src={BGImage2} alt="Background Bottom" className="background-bottom" /> */}
           </div>
 
-          {/* Event Cards Content */}
-          <div className="tab-content">{renderContent()}</div>
-        </main>
+          <div className="events">
+            <img src={Youthopia} alt="logo" className='youthopia-logo'></img>
+            <header>EVENTS {props.name}</header>
+            <p>These are our main Events.</p>
+
+            <main className="clubs">
+              {/* Tab Buttons */}
+              <div className="tab-buttons">
+                {Object.keys(eventTypes).map((tab) => (
+                  <button
+                    key={tab}
+                    className={activeTab === tab ? 'active' : ''}
+                    onClick={() => setActiveTab(tab)}
+                    disabled={activeTab === tab}
+                    aria-label={`Show ${tab} events`}
+                  >
+                    {tab}
+                  </button>
+                ))}
+              </div>
+
+              {/* Event Cards Content */}
+              <div className="tab-content">{renderContent()}</div>
+            </main>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   );
 }
