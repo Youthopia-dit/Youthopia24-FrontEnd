60323ed3e64d1a9f39ed432a74090d00f3b12ea0
tanya-2002 <tanyaaggarwal2002@gmail.com>
2024-10-16 12:06:34 +0530

    Events  fetching

--- src/components/eventcard/eventcard.jsx
+++ src/components/eventcard/eventcard.jsx
@@ -1,17 +1,17 @@
-import './eventcard.css'; 
+import './eventcard.css';
 import image from './cs.png'
 
 
 const EventCard = ({ eventName, eventDate, posterUrl }) => {
-    return (
-      <div className="event-card">
-        <img src={image} alt={eventName} className="event-poster" />
-        <div className="event-details">
-          <h2>{eventName}</h2>
-          <p>{eventDate}</p>
-        </div>
+  return (
+    <div className="event-card">
+      <img src={posterUrl != "String" ? posterUrl : image} alt={eventName} className="event-poster" />
+      <div className="event-details">
+        <h2>{eventName}</h2>
+        <p>{eventDate}</p>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default EventCard;
