7eadf3193ea7120bf6f51b4b3f53deab82110bed
Navneet Kesarwani <138325061+Navneet9099@users.noreply.github.com>
2024-10-19 03:07:54 +0530

    css part not done

--- src/pages/Event/Eventpage1.jsx
+++ src/pages/Event/Eventpage1.jsx
@@ -4,6 +4,10 @@ import EventCard from '../../components/eventcard/eventcard';
 import { useEvents } from '../../store/events'
 
 import TechBack from '../../assets/tech_back.png';
+import Events from '../../assets/Events-Elements/Group 34452.png'
+import CulturalBack from '../../assets/Events-Elements/image 397.png'
+import InformalBack from '../../assets/Events-Elements/Mask group.png'
+
 
 const EventPage = () => {
   const [activeTab, setActiveTab] = useState('Technical');
@@ -47,29 +51,47 @@ const EventPage = () => {
   const renderBack = () => {
     if(activeTab === 'Technical') {
       return <>
+       <img src={Events} alt="yuthopia and events background" className='Events' />
       <div className="background-wrapper">
           <img src={TechBack} alt="Tech background" className="tech-back-image" />
-          
-
           <div className="gradient-overlay"></div>
         </div>
       </>;
+     } else if (activeTab === 'Cultural') {
+        return <>
+            <img src={Events} alt="Cultural events background" className="Events" />
+            <div className="background-wrapper">
+              <img src={CulturalBack} alt="Cultural background" className="cultural-back-image" />
+              <div className="gradient-overlay2"></div>
+            </div>
+          </>;
+      } else if (activeTab === 'Informal') {
+         return <>
+             <img src={Events} alt="Informal events background" className="Events" />
+                <div className="background-wrapper">
+                  <img src={InformalBack} alt="Informal background" className="informal-back-image" />
+                  <div className="gradient-overlay3"></div>
+                </div>
+              </>;
+              
+
+
     }
   }
 
   return (
     <div className="events-list-page">
-       <header>EVENTS</header>
+       
 
       <div className="event-page-background">
-        {renderBack()}
+       {renderBack()}
       </div>
      
       <div className="clubs">
         <div className="tab-buttons">
-          <button onClick={() => handleTabChange('Technical')}>Technical</button>
-          <button onClick={() => handleTabChange('Cultural')}>Cultural</button>
-          <button onClick={() => handleTabChange('Informal')}>Informal</button>
+          <button onClick={() => handleTabChange('Technical')}>TECHNICAL</button>
+          <button onClick={() => handleTabChange('Cultural')}>CULTURAL</button>
+          <button onClick={() => handleTabChange('Informal')}>INFORMAL</button>
         </div>
         <div className="event-tab-content">
           {renderContent()}
