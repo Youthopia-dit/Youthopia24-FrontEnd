eb29f69b4666d3a4c3e7696c4e3e792e3b730fc0
Navneet Kesarwani <138325061+Navneet9099@users.noreply.github.com>
2024-10-23 14:28:05 +0530

    profile

--- src/pages/Profilepage/profilepage.jsx
+++ src/pages/Profilepage/profilepage.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 import './profilepage.css';
 function ProfilePage(){
+    
     return(
         <div className="ProfilePage">
                 <div className="info">
                     <div>
-                    <div className="image" >
-                        <h3 className="heading">Profile photo</h3>
+                    <h1 className="heading1">PROFILE</h1>
                     </div>
+                    <div className="image" >
+                        <div className="addpicture">
+                            <button>+</button>
+                            <p>Add profile picture</p>
+                        </div>
+                        
                     <div className="contact">
                         <h3 className="contactinfo">Phone: 1234567890 <br /> Email: example@gmail.com</h3>
                     </div>
                     </div>
                     <div className="mainheading">
-                        <div className="bgplate"><h2 className="text"> Your Name : Name</h2></div>
+                        <div className="bgplate"><h2 className="text"> Your Name: </h2></div>
                         {/* <h3>Name</h3> */}
-                        <div className="bgplate"><h2 className="text"> College: DIT</h2></div>
+                        <div className="bgplate"><h2 className="text"> College: </h2></div>
                         {/* <h3>DIT</h3> */}
-                        <div className="bgplate"><h2 className="text"> College ID: ID</h2></div>
+                        <div className="bgplate"><h2 className="text"> College ID:</h2></div>
                         {/* <h3>ID</h3> */}
-                        <div className="bgplate"><h2 className="text"> Branch: CSE</h2></div>
+                        <div className="bgplate"><h2 className="text"> Branch: </h2></div>
                         {/* <h3>CSE</h3> */}
-                        <div className="bgplate"><h2 className="text"> Year: 2nd</h2></div>
+                        <div className="bgplate"><h2 className="text"> Year:</h2></div>
                     </div>
+                    <div className="probutton">
+                        <div className="logout">
+                        <button>LOGOUT</button> 
+                        </div>
+                        <div className="payment">
+                            <button>Proceed to Pay</button>
+                        </div>
+                         </div>
                 </div>
                 <br />
                 <br />
