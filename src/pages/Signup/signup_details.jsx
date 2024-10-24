a4dc6e1633ee73982d1dce75dcfedafee821d31d
TheDarkLord100 <miracleyt404@gmail.com>
2024-10-24 09:19:19 +0530

    Signup fixes and OTP verification

--- src/pages/Signup/signup_details.jsx
+++ src/pages/Signup/signup_details.jsx
@@ -27,7 +27,7 @@ function SignIn() {
     if (data.college === 'DIT University') {
       setIsCollege(true);
     }
-  }, []);
+  }, [data]);
 
   const handleChange = (e) => {
     setFormData({ ...formdata, [e.target.name]: e.target.value });
@@ -38,10 +38,10 @@ function SignIn() {
     console.log(formdata);
     try {
       console.log("Test");
-      const res = await axios.post('https://27.123.248.68:4000/api/user/initialsignup', formdata);
+      const res = await axios.post('http://localhost:4000/api/user/initialsignup', formdata);
       console.log(res);
       if (res.status === 201) {
-        navigate('/');
+        // navigate('/');
         setSnackbarMessage('Sign up successful!');
         setSnackbarSeverity('success');
         setSnackbarOpen(true);
